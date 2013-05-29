7e681e0809a63d193e87743646af42aac7b735d3
Dylan <dxa4481@rit.edu>
2013-05-29 03:57:26 -0400

    So I started the algorithm and finished turning the 16 words into 64 words

--- sha256vhdl/sha256/messagechunk.vhd
+++ sha256vhdl/sha256/messagechunk.vhd
@@ -24,14 +24,42 @@ use IEEE.NUMERIC_STD.ALL;
 --This takes a 512 chunk and returns the hash for it
 
 entity messagechunk is
-		Port(messagechunk: in STD_LOGIC_VECTOR(511 downto 0);
+		Port(	clk: in STD_LOGIC;
+				messagechunk: in STD_LOGIC_VECTOR(511 downto 0);
 				chunkhash: out STD_LOGIC_VECTOR(255 downto 0));
 end messagechunk;
 
 architecture Behavioral of messagechunk is
 
+		type arrayofvectors63 is array(0 to 63) of STD_LOGIC_VECTOR(31 downto 0) ;
+		type harray is array(0 to 7) of STD_LOGIC_VECTOR(31 downto 0);
+		signal h : harray:= (x"6a09e667", x"bb67ae85", x"3c6ef372", x"a54ff53a", x"510e527f", x"9b05688c", x"1f83d9ab", x"5be0cd19");
+		signal wordsext : arrayofvectors64;
 begin
+		process(clk)
+			begin
+			if (clk'event and clk = '1') then
+				for I in 0 to 15 loop
+					wordsext(I) <= messagechunk(511-I*32 downto 480-I*32);
+				end loop;
+			end if;
+		end process;
+		
 
+		process(clk)
+			variable s0,s1  	: std_logic_vector (31 downto 0);
+			variable vwords : arrayofvectors64;
+			begin
+			if (clk'event and clk = '1') then
+				vwords := wordsext;
+				for i in 16 to 63 loop
+					s0 := (vwords(i-15) sra 7) xor (vwords(i-15) sra 18) xor (vwords(i-15) srl 3);
+					s1 := (vwords(i-2) sra 17) xor (vwords(i-2) sra 19) xor (vwords(i-2) srl 10);
+					vwords(i):=vwords(i-16)+s0+vwords(i-7)+s1;
+				end loop;
+				wordsext <= vwords;
+			end if;
+		end process;
 
 end Behavioral;
 
