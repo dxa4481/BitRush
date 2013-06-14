c2a4fe3983035ac879106f5a5f2462fb88d7b7a5
dxa4481 <dxa4481@rit.edu>
2013-06-14 06:56:18 -0400

    Added comments, made it look a little neater

--- operators.py
+++ operators.py
@@ -5,51 +5,72 @@ add, multiply by 2 and divide by 2
 
 
 def devideby2(num):
-    return((num/2)%4294967296)
+    """
+    takes an int, divides it by 2
+    """
+    return((num / 2) % 4294967296)
+    
 def multby2(num):
-    return((num*2)%4294967296)
+    """
+    takes an int multiplies it by 2
+    """
+    return((num * 2) % 4294967296)
+    
 def add(num1,num2):
-    return((num1+num2)%4294967296)
-
-
-
+    """
+    adds 2 numbers
+    """
+    return((num1 + num2) % 4294967296)
 
 def shiftleft(num,shiftplaces):
+    """
+    shifts left by shift places takes an int
+    """
     for i in range(shiftplaces):
         num = multby2(num)
     return num
 
 def shiftright(num,shiftplaces):
+    """
+    shifts right by shiftplaces, takes an int
+    """
     for i in range(shiftplaces):
         num = devideby2(num)
     return num
 
 def rotateright(num,rotateplaces):
+    """
+    rotates right by rotate places, takes an int
+    """
     shifted=shiftright(num,rotateplaces)
     mask=add(shiftleft(2,rotateplaces-1),-1)
     masked=andbits(num,mask)
-    shiftedmask=shiftleft(masked,31-rotateplaces)
+    shiftedmask=shiftleft(masked,31 - rotateplaces)
     addon=add(shiftedmask,shifted)
     return addon
+    
 def setbit(num,bit):
     """ set bit makes all numbers 0 that aren't the bit of interest
     bits are numbered like this 31...3,2,1,0
     """
     num=shiftright(num,bit)
     num=shiftleft(num,31)
-    num=shiftright(num,31-bit)
+    num=shiftright(num,31 - bit)
     return num
 
 def andbits(num1,num2):
+    """
+    ands 2 numbers takes ints
+    """
     thingstoadd=[]
     for i in range(31):
         bit1=setbit(num1,i)
         bit2=setbit(num2,i)
-        bit1=shiftleft(bit1,30-i)
-        bit2=shiftleft(bit2,30-i)
+        bit1=shiftleft(bit1,30 - i)
+        bit2=shiftleft(bit2,30 - i)
         bitsum=add(bit1,bit2)
         bitsum=setbit(bitsum,31)
-        bitsum=shiftright(bitsum,31-i)
+        bitsum=shiftright(bitsum,31 - i)
         thingstoadd.append(bitsum)
     bit1=setbit(num1,31)
     bit2=setbit(num2,31)
@@ -61,17 +82,23 @@ def andbits(num1,num2):
     return sum(thingstoadd)
 
 def xorbits(num1,num2):
-    thingstoadd=[]
+    """
+    xors 2 numbers, takes ints
+    """
+    thingstoadd = []
     for i in range(31):
         bit1=setbit(num1,i)
         bit2=setbit(num2,i)
-        bit1=shiftleft(bit1,31-i)
-        bit2=shiftleft(bit2,31-i)
+        bit1=shiftleft(bit1,31 - i)
+        bit2=shiftleft(bit2,31 - i)
         bitsum=add(bit1,bit2)
-        bitsum=shiftright(bitsum,31-i)
+        bitsum=shiftright(bitsum,31 - i)
         thingstoadd.append(bitsum)
     return sum(thingstoadd)
 
+"""
+code below is for testing
+"""
 print(shiftleft(shiftright(shiftleft(17,20),21),14))
-print(shiftleft(shiftright(shiftleft(17,20),21),14))+(shiftleft(shiftright(shiftleft(12,7),1),20))
+print(shiftleft(shiftright(shiftleft(17,20),21),14)) + (shiftleft(shiftright(shiftleft(12,7),1),20))
 
